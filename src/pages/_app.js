dc7457d6a1eb0f0d0eeb2cccfb01a65ec15aaad6
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-03-30 03:33:06 +0330

    Add pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,39 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+
+import { ThemeProvider, createTheme, Container } from "@mui/material";
+import { Vazirmatn } from "next/font/google";
+
+import { ApplicationLayout } from "@/layouts";
+import { Navbar } from "@/components";
+
+const vazirmatn = Vazirmatn({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#000",
+      },
+    },
+    typography: {
+      fontFamily: vazirmatn.style.fontFamily,
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <ApplicationLayout>
+        <Navbar />
+        <Container
+          sx={
+            {
+              // py: 3,
+            }
+          }
+        >
+          <Component {...pageProps} />
+        </Container>
+      </ApplicationLayout>
+    </ThemeProvider>
+  );
 }
