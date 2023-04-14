ce2a35aaea4c63d75f63b955a836b6bfd80c0dbe
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-14 17:23:49 +0330

    Fix space

--- src/pages/index.js
+++ src/pages/index.js
@@ -106,6 +106,7 @@ export default function Home({ services, error }) {
                       ))}
                     </Grid>
                   )}
+                  <br />
                 </Box>
               ))
             ) : (
